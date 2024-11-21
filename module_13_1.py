e421a051c7ce3cd2b1836951837820f20af992d8
Krekerdgek <dimos-79@mail.ru>
2024-11-21 20:27:27 +0300

    Update module_13_1.py

--- module_13_1.py
+++ module_13_1.py
@@ -1,25 +1,20 @@
 # Задача "Асинхронные силачи":
 import asyncio
 
-async def start_strongman(name, power):
+async def start_strongman(name, power, time_coeff = 2.0):
     print(f'Силач {name} начал соревнования.')
-    for i in range(5):
-        number = i + 1
-        await asyncio.sleep(i*5/power)
-        print(f'Силач {name} поднял {number}шар')
-
+    for nball in range(5):
+        await asyncio.sleep(time_coeff / power)
+        print(f'Силач {name} поднял {nball+1} шар')
     print(f'Силач {name} закончил соревнования.')
 
 async def start_tournament():
+    task1 = asyncio.create_task(start_strongman('Pasha', 3))
+    task2 = asyncio.create_task(start_strongman('Denuis', 4))
+    task3 = asyncio.create_task(start_strongman('Apollon', 5))
+    await task1
+    await task2
+    await task3
 
-    time_1= asyncio.create_task(start_strongman('Pasha', 3))
-    time_2 = asyncio.create_task(start_strongman('Denis', 4))
-    time_3 = asyncio.create_task(start_strongman('Appolon', 5))
-
-    await time_1
-    await time_2
-    await time_3
-
-
-
-asyncio.run(start_tournament())
+if __name__ == '__main__':
+    asyncio.run(start_tournament())
